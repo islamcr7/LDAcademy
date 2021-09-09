25528b31446fd933950bffb646c799a03a60d2c7
hammoudii <hammoudi.islam@live.fr>
2021-09-09 01:27:20 +0100

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Resources\TodoResource as TodoResource;
+use App\Http\Controllers\BaseController as BaseController;
+
+class TodoController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos=Todo::all();
+
+        return $this->sendResponse(TodoResource::collection($todos), 'Todos retrieved successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $Todo = Todo::create($request->all());
+   
+        return $this->sendResponse(new TodoResource($Todo), 'Todo created successfully.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return $this->sendResponse(new TodoResource($todo), 'Todo retrieved successfully.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+      
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $todo->update($request->all());   
+ 
+        return $this->sendResponse(new TodoResource($todo), 'Todo retrieved successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return $this->sendResponse([], 'todo deleted successfully.');
+    }
+}
